966909e4d756fc8cb5c46056a7c8a38dbcc935d4
Kai <kkarzhauov4@gmail.com>
2023-06-12 20:39:51 +0500

    Optimize FlatList, add infinite scroll

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,19 +1,58 @@
 import * as React from 'react';
-import {ActivityIndicator, View} from 'react-native';
+import {ActivityIndicator, ListRenderItem, View} from 'react-native';
 
 import {styles} from './styles';
 import {useUsers} from '@hooks';
 import {Item, List} from '@components';
+import {getItemLayout} from '@utils';
 
 const HomeScreen = () => {
-  const {isLoading, error, data: users} = useUsers();
+  const {
+    isLoading,
+    error,
+    data,
+    isRefetching,
+    hasNextPage,
+    refetch,
+    fetchNextPage,
+  } = useUsers();
 
   if (isLoading && !error) {
     return <ActivityIndicator />;
   }
+
+  if (!data) {
+    return null;
+  }
+
+  const users = data.pages.map(page => page.results).flat();
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
+
+  const onRefetch = () => {
+    refetch();
+  };
+
+  const renderItem: ListRenderItem<any> = ({item}) => <Item item={item} />;
+
   return (
     <View style={styles.screen}>
-      <List renderItem={({item}) => <Item item={item} />} data={users} />
+      {users.length > 0 && (
+        <List
+          refreshing={isRefetching}
+          onRefresh={onRefetch}
+          onEndReached={loadMore}
+          data={users}
+          initialNumToRender={10}
+          getItemLayout={getItemLayout}
+          renderItem={renderItem}
+          onEndReachedThreshold={0.2}
+        />
+      )}
     </View>
   );
 };
